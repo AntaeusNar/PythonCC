ae0e936d0ffa991e458e65af609c9a18da04c96f
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-10-11 10:27:32 -0700

    finished out basic strings

---
+++ name.py
@@ -0,0 +1,10 @@
+first_name = "jed"
+middle_name = "holmes"
+last_name = "wallace"
+full_name = first_name + " " +middle_name + " " + last_name
+print(full_name.title())
+
+print(full_name.upper())
+print(full_name.lower())
+
+print("Hello, " + full_name.title())
