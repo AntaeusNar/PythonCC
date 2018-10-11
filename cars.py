7417c9e62d08f2d1e359fece65212115b6a82e45
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-10-11 11:37:27 -0700

    Finished chapter 5

--- cars.py
+++ cars.py
@@ -1,7 +1,17 @@
+
 cars = ['audi', 'bmw', 'subaru', 'toyota']
 
 for car in cars:
     if car == 'bmw':
         print(car.upper())
+    elif car != 'subaru':
+        print(car)
     else:
         print(car.title())
+
+if 'bmw' in cars:
+    print("Honey, I am home!!")
+elif 'telsa' not in cars:
+    print("We are all going to die")
+else:
+    print("you know, you might be doing life wrong")
