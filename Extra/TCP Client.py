cef768301141551143074d1e7e6bb1953a9716d2
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-10-12 08:59:08 -0700

    Starting work on TCP/IP Communication

---
+++ Extra/TCP Client.py
@@ -0,0 +1,1 @@
+# Trying to learn TCP communications, this is going to be the 'client' or IoT device
---
+++ Extra/TCP Sever.py
@@ -0,0 +1,1 @@
+# Trying to learn TCP communications, this is going to be the 'server' or IoT device monitor
