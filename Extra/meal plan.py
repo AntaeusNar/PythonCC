8edb5bf311b55087234abd932d3c06a0166612f2
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-10-15 14:13:37 -0700

    working with the lists and data for formating usfulness

--- Extra/meal plan.py
+++ Extra/meal plan.py
@@ -5,3 +5,42 @@
 # of the week
 
 # functions for inputing new meals, buiding meal plans, and building the shopping list
+from typing import Any, Tuple, List
+
+# days of the week
+# days = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+
+# list for all the meals
+meals = []
+
+# dictonary for all the ingredients with the ingredants as the key and the number of meal recipes using the ingredants
+# as the value
+# ingredients = {}
+
+# individual meals will be a dictonay with a name and then the ingredients to allow for adding more things later
+# meal = {'name' : '',
+#         'ingredients' : ['', '']
+#        }
+
+
+
+def add_meal():
+    meal = {'name': '', 'ingredients': []}
+    prompt = 'Add a meal by typing a list seperated by commas.  The first item in the list will be the meal ' \
+             'and the rest will be the ingredients. You will have a chance to review before ' \
+             'saving. Enter q to go back.\n: '
+    message = input(prompt)
+    if message != 'q':
+        stripedwords = [x.strip() for x in message.split(",")]
+        words = [x.lower() for x in stripedwords]
+        meal['name'] = words[0]
+        meal['ingredients'] = words[1:]
+        reply = 'So you want to add a meal called {} with {} ingredients \n' \
+                'and these ingredients are {}.\n' \
+                'If so enter "y" otherwise enter "n"'\
+                .format(meal['name'].upper(), len(meal['ingredients']), ', '.join(meal['ingredients']))
+        print(reply)
+
+
+add_meal()
+
