c7d7d22a9f74ff5a3bb0e582f8126f28135b0600
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-10-15 11:20:57 -0700

    Start on the Meal Plan!

---
+++ Extra/meal plan.py
@@ -0,0 +1,7 @@
+# attempt at a meal planning app of sorts with the ability to generate a weekly meal plan and
+# ingredients /shopping lists.
+
+# we will need a list of all of the meals, ingrediants of each meal, a main list of all ingrediants, a list of the days
+# of the week
+
+# functions for inputing new meals, buiding meal plans, and building the shopping list
