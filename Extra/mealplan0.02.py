252fee53a1e426ccc976eef47532a29dc8941a15
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2018-11-01 15:19:01 -0700

    changed file loading message changed file saving message built out new_meal

--- Extra/mealplan0.02.py
+++ Extra/mealplan0.02.py
@@ -22,13 +22,14 @@ class MealList:  # Generate a meal list class holding the methods etc for making
             with open(self.mealfile) as f_obj:
                 self.meals = json.load(f_obj)
         except FileNotFoundError:
-            print('No saved meals found.')
+            print('Save file %s not found.' % self.mealfile)
         else:
             print('Loaded %d meals.' % len(self.meals))
 
     def save(self):  # Should save the file
         with open(self.mealfile, 'w') as f_obj:
             json.dump(self.meals, f_obj)
+        print('Saved %d meals to file %s' % (len(self.meals), self.mealfile))
 
     def search(self, mealname):  # look for meal in list, return true and list of ingredients
         for meal in self.meals:
@@ -47,45 +48,67 @@ class MealList:  # Generate a meal list class holding the methods etc for making
             self.meals.append(meal)
 
 
-def main_menu(menu_list):
-    selection = SelectionMenu.get_selection(menu_list, "Meal Planning with Mary Poppins!")
-    return selection
-
-
-def all_done():         # Time to be done
+def all_done(these_meals):         # Time to be done
+    these_meals.save()
     return 'exit'
 
 
-def new_meal():         # add that new meal!!
-    return 'fun'
+def new_meal(these_meals):         # add that new meal!!
+    prompt = 'Add a meal by typing a list separated by commas.  The first item in the list will be the meal\n' \
+             'and the rest will be the ingredients. You will have a chance to review before ' \
+             'saving. Enter "b" to go back.\n: '
+    message = input(prompt)
+    if message != 'b':
+        stripedwords = [x.strip() for x in message.split(",")]
+        wordslist = [x.lower() for x in stripedwords]
+        if len(wordslist) > 2:
+            reply = 'So you want to add a meal called {} with {} ingredients ' \
+                    'which are\n{} and {}.\n' \
+                    'If so enter "y" otherwise enter "n"\n:' \
+                .format(wordslist[0].title(),
+                        (len(wordslist)-1),
+                        ', '.join(wordslist[1:-1]),
+                        wordslist[-1]
+                        )
+            message = input(reply)
+            if message == 'y':
+                these_meals.new(wordslist)
+        else:
+            print('Not a very complex meal is it? Not many ingredients that one.')
 
 
-def all_meals():        # list all those fine meals!
+def all_meals(these_meals):        # list all those fine meals!
     return 'all'
 
 
-def search_meals():     # Hunting for that perfect dish
+def search_meals(these_meals, meal):     # Hunting for that perfect dish
     return 'code'
 
 
-def build_plan():       # give me a meal plan!
+def build_plan(these_meals):       # give me a meal plan!
     return 'fish'
 
 
-def select_function(selection, switcher_list):
+def select_function(selection, switcher_list, these_meals):
     # making a list of functions that will be called
     switcher = dict(enumerate(switcher_list))
     # now we pull the function from the switcher
     func = switcher.get(selection)
     # call the function!!!!!!!!!!
-    return func()
+    return func(these_meals)
+
+
+def main_menu(menu_list):
+    selection = SelectionMenu.get_selection(menu_list, "Meal Planning with Mary Poppins!")
+    return selection
 
 
 def __main__():
+    these_meals = MealList('meals.json')
     switcher_list = [new_meal, all_meals, search_meals, build_plan, all_done]
     menu_list = ["New Meal", "All Meals", "Search Meals", "Build that Meal Plan!"]
     while True:
-        if select_function(main_menu(menu_list), switcher_list) == 'exit':
+        if select_function(main_menu(menu_list), switcher_list, these_meals) == 'exit':
             break
 
 
